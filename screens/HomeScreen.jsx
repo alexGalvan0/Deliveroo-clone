dde99562944920590cb06f7b50e7ea68160ab7d6
Alex <galvan.alex121@gmail.com>
2022-12-28 08:58:59 -0500

    update

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -5,7 +5,7 @@ import {
     UserIcon,
     ChevronDownIcon,
     MagnifyingGlassIcon,
-    AdjustmentIcon
+    AdjustmentsHorizontalIcon
 } from "react-native-heroicons/outline"
 
 const HomeScreen = () => {
@@ -19,30 +19,28 @@ const HomeScreen = () => {
     }, [])
     return (
         <SafeAreaView className='bg-white'>
-            <Text className="text-red-500">
-                {/* header */}
-                <View className="flex-row pb-3 items-center mx-4 space-x-2">
-                    <Image
-                        source={{
-                            uri: 'https://links.papareact.com/wru'
-                        }}
-                        className="h-7 w-7 bg-gray-300 p-4 rounded-full" />
-                    <View className='flex-1'>
-                        <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
-                        <Text className='font-bold text-xl'>Current Location
-                            <ChevronDownIcon size={20} color="#00CBB" />
-                        </Text>
-                    </View>
-                    <UserIcon size={35} color="#00CBB" />
+            {/* header */}
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                <Image
+                    source={{
+                        uri: 'https://links.papareact.com/wru'
+                    }}
+                    className="h-7 w-7 bg-gray-300 p-4 rounded-full" />
+                <View className='flex-1'>
+                    <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+                    <Text className='font-bold text-xl'>Current Location
+                        <ChevronDownIcon size={20} color="#00CBB" />
+                    </Text>
                 </View>
-                <View>
-                    <View className='flex-row'>
-                        <MagnifyingGlassIcon />
-                        <TextInput placeholder='Restaurants and cusinies' keyboardType='default' />
-                    </View>
-
+                <UserIcon size={35} color="#00CBB" />
+            </View>
+            <View className='flex-row items-center space-x-2 pb-2 mx-4 px-4'>
+                <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+                    <MagnifyingGlassIcon color='gray' size={20} />
+                    <TextInput placeholder='Restaurants and cusinies' keyboardType='default' />
                 </View>
-            </Text>
+                <AdjustmentsHorizontalIcon />
+            </View>
         </SafeAreaView>
     )
 }
